87381e15d0c7373f79c753718d9c4d73f0639a13
Ryota Yokote <yokote.ryota@donuts.ne.jp>
2017-01-25 21:27:29 +0900

    add namespace

--- Assets/Scripts/GestureAreaManager.cs
+++ Assets/Scripts/GestureAreaManager.cs
@@ -5,24 +5,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 
-public class GestureAreaManager : MonoBehaviour {
-  public GameObject targetPanel;
-  public RectTransform[] rects;
+namespace CommonGesture {
+  public class GestureAreaManager : MonoBehaviour {
+    public GameObject targetPanel;
+    public RectTransform[] rects;
 
-  void Start() {
-    rects = targetPanel
-      .GetComponentsInChildren<Image> (false)
-      .Where (i => i.enabled)
-      .Where (i => i.raycastTarget)
-      .Select (i => i.GetComponent<RectTransform> ())
-      .ToArray ();
-  }
-
-  public bool IsInTouchArea(Vector2 scrPos) {
-    for (int i = 0; i < rects.Length; i++) {
-      if (RectTransformUtility.RectangleContainsScreenPoint(rects[i], scrPos)) return false;
+    void Start() {
+      rects = targetPanel
+          .GetComponentsInChildren<Image>(false)
+          .Where(i => i.enabled)
+          .Where(i => i.raycastTarget)
+          .Select(i => i.GetComponent<RectTransform>())
+          .ToArray();
     }
-    return true;
-  }
 
+    public bool IsInTouchArea(Vector2 scrPos) {
+      for (int i = 0; i < rects.Length; i++) {
+        if (RectTransformUtility.RectangleContainsScreenPoint(rects[i], scrPos)) return false;
+      }
+      return true;
+    }
+
+  }
 }
